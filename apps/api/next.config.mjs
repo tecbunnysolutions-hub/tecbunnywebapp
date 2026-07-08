18f3bde3193582a9c723f5687b7cd9e07313ffde
ShubhamBhisaji <bhisaji1998@gmail.com>
2026-07-08 14:07:11 +0530

    fix(api): rely on nextjs serverExternalPackages entirely

--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['pdfkit', 'fontkit', 'sharp', '@aws-sdk/client-s3'],
+  serverExternalPackages: ['pdfkit', 'fontkit', 'sharp', '@aws-sdk/client-s3', 'nodemailer', 'bullmq'],
   experimental: {
     instrumentationHook: true,
     optimizePackageImports: ["@tecbunny/core"]
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const ext = ['pdfkit', 'fontkit', 'sharp', '@aws-sdk/client-s3'];
-      if (Array.isArray(config.externals)) {
-        config.externals.push(...ext);
-      } else if (config.externals) {
-        config.externals = [config.externals, ...ext];
-      } else {
-        config.externals = ext;
-      }
-    }
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
