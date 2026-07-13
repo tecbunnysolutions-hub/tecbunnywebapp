663cc643f93161ce4010e6ad8ff01f212b8f7b01
ShubhamBhisaji <bhisaji1998@gmail.com>
2026-07-13 21:02:56 +0530

    chore: remove test file

--- test.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-async function test() {
-  const res = await fetch('https://api.tecbunny.com/api/health');
-  const text = await res.text();
-  console.log('Status:', res.status);
-  console.log('Response:', text);
-}
-test();
